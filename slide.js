cd8724944389ce213736880361e8184e4adc6784
Bonnie Gabrielson <bonniegabrielson@gmail.com>
2019-08-19 22:24:49 -0500

    trying to fix tree

--- slide.js
+++ slide.js
@@ -12,15 +12,3 @@ function carousel() {
   x[myIndex-1].style.display = "block";
   setTimeout(carousel, 1500); // Change image every 2 seconds
 }
-
-
-
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("menu");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
